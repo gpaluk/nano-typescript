05d9a50e1c8f4ccfb31588d1004664ffa73640d0
PluginIO <garypaluk@gmail.com>
2019-05-24 19:57:01 +0100

    Added sample unit test example with karma, jest and jasmine

---
+++ karma.conf.js
@@ -0,0 +1,21 @@
+module.exports = function(config) {
+    config.set({
+        frameworks: ['jasmine', 'karma-typescript'],
+
+        basePath: 'test',
+
+        files: [
+            '**/*.ts' // *.tsx for React Jsx
+        ],
+
+        preprocessors: {
+            '**/*.ts': ['karma-typescript']
+        },
+
+        reporters: ['dots', 'karma-typescript'],
+
+        browsers: ['ChromeHeadless'],
+
+        singleRun: true
+    })
+}
