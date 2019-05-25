a16b0a1be287fdd89ed3e6da81859ffa37514b03
PluginIO <garypaluk@gmail.com>
2019-05-25 23:13:15 +0100

    Updated files based on eslint requirements

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
         '@typescript-eslint/no-unused-vars': 'off',
-        '@typescript-eslint/no-inferrable-types': 'off'
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/no-var-requires': 'off'
     }
 }
